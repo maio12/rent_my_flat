bf00bae0c9a24d3e75fc2aa3ff43ccdd7921a6f6
patricio galindo <patriciogalindo@gmail.com>
2019-12-03 17:50:29 +0100

    finished the reservations controller and new

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,6 @@
 class ReservationsController < ApplicationController
+  before_action :set_apartment, except: :destroy
+
   def index
     @reservations = Reservation.where(user: current_user)
   end
@@ -7,13 +9,28 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.find(params[:id])
   end
 
+  def new
+    @reservation = @apartment.reservations.new
+  end
+
+  def create
+    @reservation = @apartment.reservations.new(reservation_params)
+    @reservation.user = current_user
+
+    if @reservation.save
+      redirect_to apartment_path(@apartment), notice: 'Reservation was successfully created.'
+    else
+      render :new
+    end
+  end
+
   def edit
     @reservation = Reservation.find(params[:id])
   end
 
   def update
-     @reservation = Reservation.find(params[:id])
-     if @reservation.update(reservation_params)
+    @reservation = Reservation.find(params[:id])
+    if @reservation.update(reservation_params)
       redirect_to @reservation, notice: 'Succesfully updated'
     else
       render :edit
@@ -25,7 +42,11 @@ class ReservationsController < ApplicationController
     @resertion.destroy
   end
 
- def reservation_params
+  def reservation_params
     params.require(:reservation).permit(:date_in, :date_out, :user_id, :apartment_id)
   end
 end
+
+  def set_apartment
+    @apartment = Apartment.find(params[:apartment_id])
+  end
