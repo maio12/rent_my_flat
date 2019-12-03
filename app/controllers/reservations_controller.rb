70580211a3c64349fd5a4754503c2638b7f79560
gregory conti <gconti63@gmail.com>
2019-12-03 17:51:10 +0100

    getting almost complete

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,30 +1,26 @@
 class ReservationsController < ApplicationController
+  before_action :set_apartment, except: :destroy
   def new
-    @apartment = Apartment.find(params[:apartment_id])
-    @reservation = Reservation.new
+    @reservation = @apartment.Reservation.new
   end
 
   def create
     @reservation = Reservation.new(reservations_params)
     @reservation.user = current_user
     if @reservation.save
-      redirect_to root_path
+      redirect_to apartment_path
     else
       render :new
     end
-  end
-
-  def check_in_date(date)
-    @check_in_date = date
-  end
-
-  def check_out_date(date)
-    @check_out_date = date
   end
 
   private
 
   def reservations_params
     params.require(:reservation).permit(:date_in, :date_out)
+  end
+
+  def set_apartment
+    @apartment = Apartment.find(params[:apartment_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :apartments, only: [:new, :create, :show] do
-    resources :reservations, only: [:new, :create]
     resources :reviews, only: [:new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
