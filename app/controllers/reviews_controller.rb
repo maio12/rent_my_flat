426daf7b79fbfee25a7c61cdce32c46104cbac98
Antonio Maiocchi <antonio.maiocchi12@gmail.com>
2019-12-04 15:40:09 +0100

    backend-and-routing-check

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,20 +1,24 @@
 class ReviewsController < ApplicationController
   def create
-    @apartment = Apartment.find(params[:apartment_id])
+
     @review = Review.new(review_params)
+    @reservation = Reservation.find(params[:reservation_id])
+    @review.reservation = @reservation
+
     if @review.save
-      redirect_to @apartment, notice: 'Your review was successfully created'
+
+      redirect_to @reservation.apartment, notice: 'Your review was successfully created'
     else
       render :new, alert: 'Your review was not saved'
     end
   end
 
   def new
-    @apartment = Apartment.find(params[:apartment_id])
+    @reservation = Reservation.find(params[:reservation_id])
     @review = Review.new
   end
 
   def review_params
-    params.require(:review).permit(:rating, :content)
+    params.require(:review).permit(:rating, :content, :reservation_id)
   end
 end
