647d57aeac6ff2ba92546c63dad7663d46676047
gregory conti <gconti63@gmail.com>
2019-12-04 14:09:17 +0100

    gins new push

--- app/policies/apartment_policy.rb
+++ app/policies/apartment_policy.rb
@@ -3,5 +3,21 @@ class ApartmentPolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+
+    def index
+      @restaurants = policy_scope(Restaurant).order(created_at: :desc)
+    end
+
+    def create?
+      true
+    end
+
+    def update?
+      record.user == user
+    end
+
+    def destroy?
+      record.user == user
+    end
   end
 end
