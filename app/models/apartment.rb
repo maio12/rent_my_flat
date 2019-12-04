85dc63c099498e7de4855b1683b19e332923aaae
patricio galindo <patriciogalindo@gmail.com>
2019-12-04 14:07:44 +0100

    change by gin

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -4,6 +4,5 @@ class Apartment < ApplicationRecord
   has_many :reviews, through: :reservations
   validates :title, presence: true
   validates :address, presence: true
-  validates :photo, presence: true
   # mount_uploader :photo, PhotoUploader
 end
