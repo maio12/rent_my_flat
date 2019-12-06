833c3f93f9444a25d5ec55eb25463c9f9d79b986
Antonio Maiocchi <antonio.maiocchi12@gmail.com>
2019-12-06 11:28:21 +0100

    resolving conflicts 3

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,8 @@ import "../components/flatpickr"
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
 
+// CSS
+import ‘mapbox-gl/dist/mapbox-gl.css’;
+// internal imports
+import { initMapbox } from ‘../init_mapbox’;
+initMapbox();
